b16179ce55a9742c90166a21d3ceff9581ab79b5
myezo <mikeyyz315@gmail.com>
2017-02-28 23:14:45 -0500

    object function error

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,11 +1,41 @@
 var friendsData = [
   {
-    customerName: "Ahmed",
-    customerEmail: "ahmed@example.com",
-    customerID: "afhaque89",
-    phoneNumber: "000-000-0000"
+    name: "Jones",
+    image: "https://pbs.twimg.com/profile_images/494672767157952512/gk2fDcQ6.jpeg",
+    scores: [
+    		3,
+    		3,
+    		3,
+    		4,
+    		2,
+    		4,
+    		3,
+    		3,
+    		3,
+    		3
+    ]
+  },
+
+  {
+    name: "Martha",
+    image: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwij1aXbrLTSAhXCKyYKHUk7DGwQjRwIBw&url=http%3A%2F%2Fwww.businessinsider.com%2Fmartha-stewart-is-popular-on-matchcom-2013-5&bvm=bv.148441817,d.amc&psig=AFQjCNEVWaSE-vRdtvko4a1oxefZ__f5QA&ust=1488424826029049",
+    scores: [
+    		3,
+    		3,
+    		3,
+    		4,
+    		2,
+    		4,
+    		3,
+    		3,
+    		3,
+    		3
+    ]
   }
 ];
+
+
+
 
 // Note how we export the array. This makes it accessible to other files using require.
 module.exports = friendsData;
